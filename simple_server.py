6d765b360da4718da534f9b2a799996c896e6f08
kingofsweets <kingofsweetsx2k@gmail.com>
2022-02-18 23:24:43 +0300

    - new server update

--- simple_server.py
+++ simple_server.py
@@ -1,33 +1,55 @@
-import socket, time
+import threading
+import socket
 
 host = socket.gethostbyname(socket.gethostname())
 port = 9090
 
-clients = []
-
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind((host,port))
 
-quit = False
-print("[ Server Started ]")
+s.listen()
 
-while not quit:
-	try:
-		data, addr = s.recvfrom(1024)
+clients = []
+nicknames = []
 
-		if addr not in clients:
-			clients.append(addr)
+def trans(mess):
+    for client in clients:
+        client.send(mess)
+        
+def handle_connection(client): 
+    quit = False
+    print("[ Server Started ]")
+    while not quit:
+        try:
+            message = client.recv(1024)
+            trans(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            nicknames.remove(nicknames[index])
+            trans(f"{nicknames[index]} left the chat.".encode('utf-8'))
+            quit = True
 
-		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
-
-		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="")
-		print(data.decode("utf-8"))
-
-		for client in clients:
-			if addr != client:
-				s.sendto(data,client)
-	except:	
-		print("\n[ Server Stopped ]")
-		quit = True
-		
-s.close()
+def main():
+    print("SERVER POSHOLLL...")
+    while True:
+        client, addr = s.accept()
+        print(f"Connected to {addr}")
+        
+        client.send("NICK".encode('utf-8'))
+        
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+        
+        print(f"Nick is {nickname}")
+        
+        trans(f"{nickname} joined the chat.".encode('utf-8'))
+        
+        client.send("You are now connected".encode('utf-8'))
+        
+        thread = threading.Thread(target= handle_connection, args=(client,))
+        thread.start()
+        
+if __name__ == '__main__':
+    main()
